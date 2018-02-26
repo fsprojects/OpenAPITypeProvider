ba996c23d8241f979c34d7d4dfd9d666faf7be33
Roman Provazník <dzoukr@dzoukr.cz>
2018-02-26 08:06:55 +0100

    Playing with generics

--- src/OpenAPITypeProvider/Types/Schema.fs
+++ src/OpenAPITypeProvider/Types/Schema.fs
@@ -17,8 +17,8 @@ let getNumberType = function
 
 let getStringType = function
     | StringFormat.String | StringFormat.Password -> typeof<string>
-    | StringFormat.Byte -> typeof<byte> //makeProperty<byte> (fun _ -> <@@ () @@>) name :> MemberInfo
-    | StringFormat.Binary -> typeof<int> // (fun _ -> <@@ () @@>) name :> MemberInfo
+    | StringFormat.Byte -> typeof<byte>
+    | StringFormat.Binary -> typeof<int>
     | StringFormat.Date | StringFormat.DateTime -> typeof<DateTime>
     
 let rec getType = function
@@ -28,8 +28,14 @@ let rec getType = function
     | String format -> format |> getStringType
     | Array schema -> 
         let typ = schema |> getType
-        let obj = Array.CreateInstance(typ, 1)
-        obj.GetType()
+        ProvidedTypeBuilder.MakeGenericType (typedefof<Option<_>>, [typ])
+        //let obj = Array.CreateInstance(typ, 1)
+        //obj.GetType()
+
+let some (typ:Type) arg =
+    let unionType = typedefof<option<_>>.MakeGenericType typ
+    let meth = unionType.GetMethod("Some")
+    Microsoft.FSharp.Quotations.Expr.Call(meth, [arg])
 
 let rec getMembers asm ns (parent:ProvidedTypeDefinition) name (schema:Schema) =
     match schema with
@@ -44,4 +50,7 @@ let rec getMembers asm ns (parent:ProvidedTypeDefinition) name (schema:Schema) =
     | Integer _ -> schema |> getType |> makeTypedProperty (fun _ -> <@@ () @@>) name :> MemberInfo
     | Number _ -> schema |> getType |> makeTypedProperty (fun _ -> <@@ () @@>) name :> MemberInfo
     | String _ -> schema |> getType |> makeTypedProperty (fun _ -> <@@ () @@>) name :> MemberInfo
-    | Array _ -> schema |> getType |> makeTypedProperty (fun _ -> <@@ () @@>) name :> MemberInfo    
+    | Array _ -> 
+        let v = some typeof<string> (Microsoft.FSharp.Quotations.Expr.Value("x"))
+        //let values = Option<string>.Some("x")
+        schema |> getType |> makeTypedProperty (fun _ -> v ) name :> MemberInfo    
