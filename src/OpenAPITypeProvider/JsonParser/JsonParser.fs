c4bc6cb521047059c60a61760164c1aa8051c868
Roman Provazník <dzoukr@dzoukr.cz>
2018-03-05 10:02:02 +0100

    Json parser by schema

--- src/OpenAPITypeProvider/JsonParser/JsonParser.fs
+++ src/OpenAPITypeProvider/JsonParser/JsonParser.fs
@@ -1,28 +1,21 @@
 module OpenAPITypeProvider.JsonParser.JsonParser
 
+open System
 open OpenAPITypeProvider.Specification
 open Newtonsoft.Json.Linq
 
-let tryCast castFn = 
-    try
-        castFn() |> ignore
-        true
-    with _ -> false
-
-
-let isSchemaAndJTokenCompatible (json:JToken) (schema:Schema) =
+let parseForSchema (schema:Schema) (json:JToken) =
     match schema with
-    | Integer Int32 -> tryCast (fun _ -> json.Value<int32>())
-    | Integer Int64 -> tryCast (fun _ -> json.Value<int64>())
+    | Integer Int32 -> json.Value<int32>() |> box
+    | Integer Int64 -> json.Value<int64>() |> box
+    | Number NumberFormat.Double -> json.Value<double>() |> box
+    | Number NumberFormat.Float -> json.Value<float32>() |> box
     | String StringFormat.String 
     | String StringFormat.Binary 
-    | String StringFormat.Password
-        -> tryCast (fun _ -> json.Value<string>())
-    | String StringFormat.Byte -> tryCast (fun _ -> json.Value<byte>())
+    | String StringFormat.Password -> json.Value<string>() |> box
+    | String StringFormat.Byte -> json.Value<byte>() |> box
     | String StringFormat.DateTime
-    | String StringFormat.Date  
-        -> tryCast (fun _ -> json.Value<string>())
-        
+    | String StringFormat.Date -> json.Value<DateTime>() |> box
 
 // let parseJToken (json:JToken) (schema:Schema) =
 //     match schema with
--- tests/OpenAPITypeProvider.Tests/JsonParser/JsonParser.fs
+++ tests/OpenAPITypeProvider.Tests/JsonParser/JsonParser.fs
@@ -1,5 +1,6 @@
 module OpenAPITypeProvider.Tests.JsonParser.JsonParser
 
+open System
 open NUnit.Framework
 open OpenAPITypeProvider.JsonParser.JsonParser
 open OpenAPITypeProvider.Tests
@@ -7,31 +8,50 @@ open Newtonsoft.Json.Linq
 open OpenAPITypeProvider.Specification
 
 [<Test>]
-let ``Integer (Int32) is compatible with JValue``() = 
+let ``Parses Integer (Int32) from JValue``() = 
     let value = 123 |> JValue
     let schema = Schema.Integer (IntFormat.Int32)
-    Assert.IsTrue(isSchemaAndJTokenCompatible value schema)
+    let expected = 123
+    Assert.AreEqual(expected, (parseForSchema schema value))
 
 [<Test>]
-let ``Integer (Int64) is compatible with JValue``() = 
+let ``Parses Integer (Int64) from JValue``() = 
     let value = 123L |> JValue
     let schema = Schema.Integer (IntFormat.Int64)
-    Assert.IsTrue(isSchemaAndJTokenCompatible value schema)
+    let expected = 123L
+    Assert.AreEqual(expected, (parseForSchema schema value))
 
 [<Test>]
-let ``String (String) is compatible with JValue``() = 
+let ``Parses String (String) from JValue``() = 
     let value = "abc" |> JValue
     let schema = Schema.String (StringFormat.String)
-    Assert.IsTrue(isSchemaAndJTokenCompatible value schema)
+    let expected = "abc"
+    Assert.AreEqual(expected, (parseForSchema schema value))
 
 [<Test>]
-let ``String (Byte) is compatible with JValue``() = 
+let ``Parses String (Byte) from JValue``() = 
     let value = "64" |> JValue
     let schema = Schema.String (StringFormat.Byte)
-    Assert.IsTrue(isSchemaAndJTokenCompatible value schema)
+    let expected = 64uy
+    Assert.AreEqual(expected, (parseForSchema schema value))
 
 [<Test>]
-let ``String (Date) is compatible with JValue``() = 
-    let value = "2018-01-01" |> JValue
+let ``Parses String (Date) from JValue``() = 
+    let value = "2018-01-02" |> JValue
     let schema = Schema.String (StringFormat.Date)
-    Assert.IsTrue(isSchemaAndJTokenCompatible value schema)
+    let expected = new DateTime(2018, 01, 02)
+    Assert.AreEqual(expected, (parseForSchema schema value))
+
+[<Test>]
+let ``Parses Number (Float) from JValue``() = 
+    let value = "1.2" |> JValue
+    let schema = Schema.Number (NumberFormat.Float)
+    let expected = 1.2f
+    Assert.AreEqual(expected, (parseForSchema schema value))
+
+[<Test>]
+let ``Parses Number (Double) from JValue``() = 
+    let value = "1.2" |> JValue
+    let schema = Schema.Number (NumberFormat.Double)
+    let expected = 1.2
+    Assert.AreEqual(expected, (parseForSchema schema value))
