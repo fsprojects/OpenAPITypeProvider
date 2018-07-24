f0c2b05bd96e4201c6b50236f2500af5b69e4ed1
Roman Provazník <dzoukr@dzoukr.cz>
2018-07-24 16:37:56 +0200

    changed package logo

--- build.fsx
+++ build.fsx
@@ -32,7 +32,7 @@ Target "Nuget" <| fun () ->
             sprintf "PackageReleaseNotes=\"%s\"" (release.Notes |> toNotes)
             "PackageLicenseUrl=\"http://github.com/dzoukr/OpenAPITypeProvider/blob/master/LICENSE.md\""
             "PackageProjectUrl=\"http://github.com/dzoukr/OpenAPITypeProvider\""
-            "PackageIconUrl=\"https://avatars2.githubusercontent.com/u/851307?v=3&amp;s=64\""
+            "PackageIconUrl=\"https://raw.githubusercontent.com/Dzoukr/OpenAPITypeProvider/master/logo.jpg"
             "PackageTags=\"F# FSharp OpenAPI Swagger TypeProvider\""
             "Copyright=\"Roman Provazník - 2018\""
             "Authors=\"Roman Provazník\""
