fd0de2c5b4858d2bc7e206ccca1c6bdc374a919b
Roman Provazník <dzoukr@dzoukr.cz>
2018-09-03 07:35:10 +0200

    Added Examples

---
+++ examples/NetCoreConsoleApp/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+open OpenAPITypeProvider
+
+type PetStore = OpenAPIV3Provider<"PetStore.yaml">
+
+[<EntryPoint>]
+let main argv =
+    let pet = PetStore.Schemas.Pet(1L, "Test")
+    sprintf "Pet with ID %i and name %s" pet.Id pet.Name |> Console.WriteLine
+    Console.ReadKey() |> ignore
+    0
